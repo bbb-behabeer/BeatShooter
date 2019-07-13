917ff3bd509886eb10548483f215b5d3cf8cf871
be116-live <be116.live@gmail.com>
2019-07-13 20:20:43 +0900

    ノートのいち調整

--- Assets/_MyAssets/Scripts/Note/Note.cs
+++ Assets/_MyAssets/Scripts/Note/Note.cs
@@ -1,4 +1,5 @@
 using System;
+using Sirenix.OdinInspector;
 using UnityEngine;
 
 namespace _MyAssets.Scripts.Note
@@ -6,6 +7,7 @@ namespace _MyAssets.Scripts.Note
     /// <summary>
     /// ノートの制御
     /// </summary>
+    [RequireComponent(typeof(AudioSource))]
     public class Note: MonoBehaviour
     {
         // ノート再生時のエフェクト
@@ -16,22 +18,49 @@ namespace _MyAssets.Scripts.Note
         [SerializeField] private int _moment;
 
         // 入力の許容範囲
-        [SerializeField] private int _range;
+        [SerializeField] private float _range;
 
         // 一小節の長さ
         [SerializeField] private float _duration = 1f;
         [SerializeField] private int _beat = 8;
-        
+
         // 経過時間
         private float _time;
         
         // 入力した
-        private bool _isAimed; 
-        
+        private bool _isAimed;
+
+        // オーディオソース
+        private AudioSource _audioSource;
+        private float _timePerBeat;
+        private float _just;
+
+        [Button("Start")]
         void Start()
         {
+            _audioSource = GetComponent<AudioSource>();
+            
+            // タイミングを取得
+            _timePerBeat = _duration / (float)_beat;
+            _just = _timePerBeat * _moment;
+            
+            // 縦幅を取得
+            var h = Screen.height;
+            var hh = h / 2f;
+
+            var k = Camera.main.orthographicSize / hh;
+            
+            var mb = (float)_moment /  (float)_beat ;
+            var y = (h * mb - hh) * k;
+
+            // 位置を初期化
+            var pos = transform.position;
+            pos.y = y;
+            transform.position = pos;
+            
+            
             // NoteManagerをシーンから取得
-        //    _noteManager = GameObject.Find("NoteManager").GetComponent<NoteManager>();
+            //    _noteManager = GameObject.Find("NoteManager").GetComponent<NoteManager>();
         }
 
         private void FixedUpdate()
@@ -39,21 +68,20 @@ namespace _MyAssets.Scripts.Note
             _time += Time.deltaTime;
 
             // タイミングを計算
-            var timePerBeat = _duration / (float)_beat;
-            var just = timePerBeat * _moment;
-            var min = just - _range;
-            var max = just + _range;
+            var min = _just - _range;
+            var max = _just + _range;
 
             // 範囲内であればエイム可能
             if (_time > min && _time < max)
             {
                 // エイムしておらず
                 // 入力があればエイムする
-                if (!_isAimed && Input.GetButtonDown("Shot"))
+                if (!_isAimed && Input.GetButtonDown("Jump"))
                 {
                     _isAimed = true;
                     
                     // エイム処理
+                    _audioSource.Play();
                     Debug.Log(gameObject);
                 }
             }
